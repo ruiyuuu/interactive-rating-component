0388f4e4e98d8750cd88ff05315cdc3828920ddc
ruiyuuu <jasmine59699@gmail.com>
2023-12-03 17:06:26 +0800

    upate meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,47 @@
 import '@/styles/globals.css'
 import 'tailwindcss/tailwind.css';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+  <Head>
+    <title>
+      Interactive-rating-component|front end challenge|next.js+Tailwind
+    </title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="front end challenge||Interactive-rating-component|front end challenge|next.js+Tailwind"
+    />
+    <meta name="author" content="Jasmine" />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;500;600&display=swap"
+      rel="stylesheet"
+    />
+    <meta
+      property="og:site_name"
+      content="Interactive-rating-component|front end challenge|next.js+Tailwind"
+    />
+    <meta
+      property="og:title"
+      content="Interactive-rating-component|front end challenge|next.js+Tailwind"
+    />
+    <meta
+      property="og:image"
+      content="https://raw.githubusercontent.com/ruiyuuu/interactive-rating-component/main/public/desktop-preview.jpg"
+    ></meta>
+    <meta
+      property="og:description"
+      content="front end challenge||Interactive-rating-component|front end challenge|next.js+Tailwind"
+    />
+    <link
+      rel="canonical"
+      href="https://interactive-rating-component-hazel-ten.vercel.app/"
+    />
+  </Head>;
   return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,26 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="zh-Hant-TW">
-      <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>
-          Interactive-rating-component|front end challenge|next.js+Tailwind
-        </title>
-        <meta
-          name="description"
-          content="front end challenge||Interactive-rating-component|front end challenge|next.js+Tailwind"
-        />
-        <meta name="author" content="Jasmine" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head/>
       <body className="bg-verydarkblue">
         <Main />
         <NextScript />
